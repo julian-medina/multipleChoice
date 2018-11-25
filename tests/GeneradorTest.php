916daa02954db54451d826233c4dc55edd031962
Ignacioe <mypromega@hotmail.com>
2018-11-25 19:31:26 -0300

    Correcion de Sintaxis

--- tests/GeneradorTest.php
+++ tests/GeneradorTest.php
@@ -4,9 +4,9 @@ use PHPUnit\Framework\TestCase;
 
 class generadorTests extends TestCase 
 {
-    public function(){
+    public function generarArchivoTests(){
         $archivo='/preguntas.yml';
         $ejemplo=new Generador($archivo);
-        this->assertEquals(0,0);
+        $this->assertEquals(0,0);
     }
 }
