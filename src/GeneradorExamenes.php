dbd5b4b63b2b78fbe8e5ff351c5400d241c3f1da
julian-medina <42211754+julian-medina@users.noreply.github.com>
2018-11-25 17:49:59 -0300

    Creada la clase Generador y sus funciones

---
+++ src/GeneradorExamenes.php
@@ -0,0 +1,20 @@
+
+class Generador {
+    protected $listaPreguntas;
+    
+	public function __construct($file){
+        $pregs_text = Yaml::parseFile($file);;
+        $pregs = $pregs_text['preguntas'];
+        shuffle($pregs);
+        for($i = 0; $i < 10; $i++){
+            $this->listaPreguntas[$i] = new Pregunta($pregs[$i], $i+1);
+        }
+    }
+   
+   public function getCantPreguntas(){
+        return count($this->listaPreguntas);
+    }
+    public function getListaPreguntas(){
+        return $this->listaPreguntas;
+    }
+}
