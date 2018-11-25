c510b1a15910e06f031c188e4d35f11203ac5fbf
Ignacioe <mypromega@hotmail.com>
2018-11-25 19:37:39 -0300

    cambio asserts del test

--- tests/GeneradorTest.php
+++ tests/GeneradorTest.php
@@ -1,12 +1,15 @@
 <?php
+
 namespace multipleChoice;
+
 use PHPUnit\Framework\TestCase;
 
-class generadorTests extends TestCase 
+class GeneradorTests extends TestCase 
 {
     public function generarArchivoTests(){
         $archivo='/preguntas.yml';
         $ejemplo=new Generador($archivo);
-        $this->assertEquals(0,0);
+        $this->assertTrue(isset($archivo));
+        $this->assertTrue(isset($ejemplo));
     }
 }
