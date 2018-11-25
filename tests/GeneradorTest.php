758ae34bb4377265c7b8ecff0dbe453dd8ce7032
Ignacioe <mypromega@hotmail.com>
2018-11-25 19:27:57 -0300

    Agrego test sin funcionalidad
    y cambio de nombres

--- tests/GeneradorTest.php
+++ tests/GeneradorTest.php
@@ -1,3 +1,12 @@
 <?php
 namespace multipleChoice;
 use PHPUnit\Framework\TestCase;
+
+class generadorTests extends TestCase 
+{
+    public function(){
+        $archivo='/preguntas.yml';
+        $ejemplo=new Generador($archivo);
+        this->assertEquals(0,0);
+    }
+}
