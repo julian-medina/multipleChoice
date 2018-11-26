4a2a180cce56ff984780c845cc6e38ad5a58932a
Ignacioe <mypromega@hotmail.com>
2018-11-25 23:11:54 -0300

    offset dinamico corregido

--- src/Generador.php
+++ src/Generador.php
@@ -5,11 +5,11 @@ use Symfony\Component\Yaml\Yaml;
 class Generador{
     protected $listaPreguntas;
     public function __construct($archivo){
-        $pregs_text = Yaml::parseFile($archivo);;
-        $pregs = $pregs_text['preguntas'];
-        shuffle($pregs);
-        for($i = 0; $i < $this->cantidadPreguntas(); $i++){
-            $this->listaPreguntas[$i] = new Pregunta($pregs[$i], $i+1);
+        $preguntasyml = Yaml::parseFile($archivo);;
+        $arraypreguntas = $preguntasyml['preguntas'];
+        shuffle($arraypreguntas);
+        for($i = 0; $i < sizeof($arraypreguntas); $i++){
+            $this->listaPreguntas[$i] = new Pregunta($arraypreguntas[$i], $i+1);
         }
     }
 
