b759539579603606a0e2ad7d583084acbc8216c1
Ignacioe <mypromega@hotmail.com>
2018-11-26 00:39:34 -0300

    limpio el html para usar nosotros

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 namespace multipleChoice;
 use Twig_Environment;
 use Twig_Loader_Filesystem;
- require_once  './vendor/autoload.php';
- $loader = new Twig_Loader_Filesystem('./indexes');
+require_once  './vendor/autoload.php';
+$loader = new Twig_Loader_Filesystem('./indexes');
 $twig = new Twig_Environment($loader);
- $prueba = new Generador('./preguntas.yml');
- echo $twig->render('index.html', ['preguntas' => $prueba->preguntas()] ); 
+$prueba = new Generador('./preguntas.yml');
+echo $twig->render('index.html', ['preguntas' => $prueba->preguntas()] ); 
