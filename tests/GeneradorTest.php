edbe0b7d2254e853b8f57446cc94c827957e8268
Ignacioe <mypromega@hotmail.com>
2018-11-25 22:40:39 -0300

    reduzco mas los casos

--- tests/GeneradorTest.php
+++ tests/GeneradorTest.php
@@ -8,12 +8,9 @@ class GeneradorTests extends TestCase
 {
     public function testGenerarArchivo(){
         $archivo='preguntas.yml';
-        $diezpreg='diezpreguntas.yml';
         $ejemplo=new Generador($archivo);
         $this->assertTrue(isset($archivo));
-        $this->assertTrue(isset($diezpreg));
         $this->assertTrue(isset($ejemplo));
-        $this->assertTrue(isset($ejemplo2));
         $this->assertEquals(25,$ejemplo->cantidadPreguntas());
         $this->assertNotEquals(10,$ejemplo->cantidadPreguntas());
     }
