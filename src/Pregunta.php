3f54d56abec2849dd826999c8f53c63d291eab62
Ignacioe <mypromega@hotmail.com>
2018-11-25 22:06:19 -0300

    index->numero

--- src/Pregunta.php
+++ src/Pregunta.php
@@ -2,7 +2,7 @@
 namespace multipleChoice;
 use Symfony\Component\Yaml\Yaml;
 class Pregunta {
-    protected $index;
+    protected $numero;
     protected $descripcion;
     protected $respuestas;
     protected $respuestas_correctas;
@@ -18,34 +18,41 @@ class Pregunta {
         $this->descripcion=$info["descripcion"];
         $this->respuestas_correctas = $info["respuestas_correctas"];
         $this->respuestas_incorrectas = $info["respuestas_incorrectas"];
-        if( count($this->respuestas_incorrectas) == 0) $this->todas = true;
-        if( count($this->respuestas_correctas) == 0) $this->ninguna = true;
         $this->ocultar_opcion_todas_las_anteriores = $info["ocultar_opcion_todas_las_anteriores"];
         $this->ocultas_opcion_ninguna_de_las_anteriores = $info["ocultas_opcion_ninguna_de_las_anteriores"];
         $this->texto_ninguna_de_las_anteriores = $info["texto_ninguna_de_las_anteriores"];
-        $this->armarRespuestas();
+        if( count($this->respuestas_incorrectas) == 0) $this->todas = true;
+        if( count($this->respuestas_correctas) == 0) $this->ninguna = true;
+        $this->crearPackRepuestas();
     }
 
-public function armarRespuestas(){
+    public function crearPackRepuestas(){
         $this->respuestas = array_merge($this->respuestas_correctas, $this->respuestas_incorrectas);
         shuffle($this->respuestas);
     }
+
     public function getRespuestas(){
         return $this->respuestas;
     }
+
     public function getCantRespuestas(){
         return count($this->respuestas);
     }
+
     public function getNumero(){
         return $this->numero;
     }
+
     public function getDescripcion(){
         return $this->descripcion;
     }
+
     public function getRespCorrectas(){
         return $this->respuestas_correctas;
     }
+
     public function getRespIncorrectas(){
         return $this->respuestas_incorrectas;
     }
+
 }
