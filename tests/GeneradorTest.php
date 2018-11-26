cbb541f0e273517b2ae929a8c7594807e702cc19
Ignacioe <mypromega@hotmail.com>
2018-11-25 22:33:43 -0300

    agrego () a llamadas de funciones

--- tests/GeneradorTest.php
+++ tests/GeneradorTest.php
@@ -15,9 +15,9 @@ class GeneradorTests extends TestCase
         $this->assertTrue(isset($diezpreg));
         $this->assertTrue(isset($ejemplo));
         $this->assertTrue(isset($ejemplo2));
-        $this->assertEquals(25,$ejemplo->cantidadPreguntas);
-        $this->assertNotEquals(10,$ejemplo->cantidadPreguntas);
-        $this->assertEquals(10,$ejemplo2->cantidadPreguntas);
-        $this->assertNotEquals(25,$ejemplo2->cantidadPreguntas);
+        $this->assertEquals(25,$ejemplo->cantidadPreguntas());
+        $this->assertNotEquals(10,$ejemplo->cantidadPreguntas());
+        $this->assertEquals(10,$ejemplo2->cantidadPreguntas());
+        $this->assertNotEquals(25,$ejemplo2->cantidadPreguntas());
     }
 }
