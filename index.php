a968f66503ca49fd7f4fe27f47f48242f365f8df
Ignacioe <mypromega@hotmail.com>
2018-11-26 00:29:01 -0300

    agrego index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+namespace multipleChoice;
+use Twig_Environment;
+use Twig_Loader_Filesystem;
+ require_once  './vendor/autoload.php';
+ $loader = new Twig_Loader_Filesystem('./indexes');
+$twig = new Twig_Environment($loader);
+ $prueba = new Generador('./preguntas.yml');
+ echo $twig->render('index.html', ['preguntas' => $prueba->preguntas()] ); 
